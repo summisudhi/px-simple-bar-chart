2cd4fd39979908dbaf794f58878e02e945c50cb1
Sirisha Aluri <sirisha.aluri@ge.com>
2016-04-06 10:54:07 -0700

    added wct.conf.js file and .travis.yml file to the repo

--- wct.conf.js
+++ wct.conf.js
@@ -1,12 +1,95 @@
 module.exports = {
-    verbose: true,
+    verbose: false,
     plugins: {
         local: {
-            browsers: ['chrome', 'firefox', 'safari']
+            browsers: ['chrome', 'firefox']
         },
         sauce: {
-            disabled: true
+             "disabled" : true,
+             "browsers": [
+             
+         {
+          "browserName": "internet explorer",
+          "platform": "Windows 7",
+          "version": "10"
+        },
+        {
+          "browserName": "chrome",
+          "platform": "Windows 10",
+          "version": "44"
+        },
+         {
+          "browserName": "microsoftedge",
+          "platform": "Windows 10"
+         
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 10",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8",
+          "version": "10"
+        },
+         {
+          "browserName": "firefox",
+          "platform": "Windows 8.1",
+          "version": "44"
+        },
+         {
+          "browserName": "chrome",
+          "platform": "Windows 8.1",
+          "version": "48"
+        },
+         {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        },
+         
+        
+         {
+          "browserName": "chrome",
+          "platform": "OS X 10.11",
+          "version": "48"
+        },
+             
+           
+        {
+          "browserName": "safari",
+          "platform": "OS X 10.11",
+          "version": "9"
+        },
+             
+         {
+          "browserName": "iphone",
+          "platform": "OS X 10.10",
+          "version": "9.2",
+          "deviceName": "iPad Retina",
+          "deviceOrientation": "portrait"
+          
+        },
+        
+        {
+          "browserName": "iphone",
+          "platform": "OS X 10.10",
+          "version": "9.2",
+          "deviceName": "iPad 2",
+          "deviceOrientation": "portrait"
+          
         }
+
+        
+        
+      ]
+    }
     },
     suites: [
       'test/px-simple-bar-chart-tests.html'
